1a8a47d33310c410f8e81847e3662102b5e9102c
João Victor Esteves <62037656+jvesteves@users.noreply.github.com>
2021-05-29 09:18:23 -0300

    Aula 5

---
+++ FDevsQuiz.Infra/Data/Repository/AlternativaRepository.cs
@@ -0,0 +1,14 @@
+﻿using FDevsQuiz.Domain.Interface;
+using FDevsQuiz.Domain.Model;
+using FDevsQuiz.Domain.Repository;
+using FDevsQuiz.Infra.Data.Repository.Base;
+
+namespace FDevsQuiz.Infra.Data.Repository
+{
+    public class AlternativaRepository : CrudRepository<long, EnqAlternativa>, IAlternativaRepository
+    {
+        public AlternativaRepository(IDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ FDevsQuiz.Infra/Data/Repository/PerguntaRepository.cs
@@ -0,0 +1,14 @@
+﻿using FDevsQuiz.Domain.Interface;
+using FDevsQuiz.Domain.Model;
+using FDevsQuiz.Domain.Repository;
+using FDevsQuiz.Infra.Data.Repository.Base;
+
+namespace FDevsQuiz.Infra.Data.Repository
+{
+    public class PerguntaRepository : CrudRepository<long, EnqPergunta>, IPerguntaRepository
+    {
+        public PerguntaRepository(IDbContext context) : base(context)
+        {
+        }
+    }
+}
